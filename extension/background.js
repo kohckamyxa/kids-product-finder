9e7c157d8ca81f24607520af0aa44e603eba198d
kohckamyxa <168310011+kohckamyxa@users.noreply.github.com>
2025-09-24 12:09:56 +0200

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,32 +1,31 @@
 // Listen for a message from the popup to start the search
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "START_SEARCH") {
-      const searchData = message.data;
-  
-      // Make the long-running fetch call to the backend
-      fetch('http://127.0.0.1:5000/api/search', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(searchData),
-      })
-      .then(response => response.json())
-      .then(data => {
-        // When the search is done, send the results back to the popup
-        chrome.runtime.sendMessage({
-          type: "SEARCH_COMPLETE",
-          data: data.results
-        });
-      })
-      .catch(error => {
-        console.error('Error in background fetch:', error);
-        // Send an empty result back in case of an error
-        chrome.runtime.sendMessage({
-          type: "SEARCH_COMPLETE",
-          data: []
-        });
+  if (message.type === "START_SEARCH") {
+    const searchData = message.data;
+
+    // Make the long-running fetch call to the live backend
+    fetch('https://kids-product-finder.onrender.com/api/search', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(searchData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      // When the search is done, send the results back to the popup
+      chrome.runtime.sendMessage({
+        type: "SEARCH_COMPLETE",
+        data: data.results
       });
-      
-      // Return true to indicate you will be sending a response asynchronously
-      return true; 
-    }
-  });
+    })
+    .catch(error => {
+      console.error('Error in background fetch:', error);
+      // Send an empty result back in case of an error
+      chrome.runtime.sendMessage({
+        type: "SEARCH_COMPLETE",
+        data: []
+      });
+    });
+    
+    return true; 
+  }
+});
